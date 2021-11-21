6936a956540f44aae1e344eb1b9d8b9f53aabcbc
dung0312-ptit <dungtn312@gmail.com>
2021-11-21 16:56:34 +0700

    order tracking done

--- OrderDetail.php
+++ OrderDetail.php
@@ -3,6 +3,30 @@ session_start();
 if(!isset($_SESSION['username'])){
     header("Location: login.php");
 }
+include 'Conn.php';
+
+$sql = "SELECT * FROM orders WHERE id='".$_GET['id']."'";
+$orders = mysqli_query($conn,$sql);
+if(false===$orders){
+    echo "<script>alert('loi tum lum')</script>";
+}
+if(mysqli_num_rows($orders) >0 ){
+    while ($row = mysqli_fetch_array($orders)){
+      $order=$row;
+    }
+}
+
+
+$sql = "SELECT * FROM status WHERE ordersID='".$order['id']."'";
+$result = mysqli_query($conn,$sql);
+if(false === $result){
+    echo "<script>alert('status errors')</script>";
+}
+if(mysqli_num_rows($result)>0){
+    while ($row = mysqli_fetch_assoc($result)){
+        $orderSta[] = $row;
+    }
+}
 ?>
 <?php
 $title = 'Chi tiết đơn hàng';
@@ -14,87 +38,9 @@ require_once 'included/navbar.php';
 ?>
 <section class="py-3">
     <div class="container px-1 mx-auto">
-        <div class="bg-white condition">
-            <div class="row d-flex justify-content-between px-3 top">
-                <div class="d-flex font-weight-bolder">
-                    <h5>Đơn Hàng <span class="text-primary font-weight-bold"><?php echo $_GET['id']?></span></h5>
-                </div>
-                <div class="d-flex flex-column text-sm-right font-weight-bolder font-italic">
-                    <p class="mb-0">Giao hàng dự kiến <span>01/12/21</span></p>
-                </div>
-            </div>
-            <!-- Add class 'active' to progress -->
-            <div class="mt-5 mx-5">
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 75%"></div>
-                </div>
-                <div class="d-flex justify-content-between mt-4 font-weight-light ">
-                    <div class="d-flex align-items-center">
-                        <i class="fa fa-check-circle-o mx-2 fa-3x" aria-hidden="true"></i>
-                        <h6 >Chờ xác nhận</h6>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <i class="fa fa-hand-o-left mx-2 fa-3x" aria-hidden="true"></i>
-                        <h6 >Đang chuẩn bị hàng</h6>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <i class="fa fa-truck mx-2 fa-3x" aria-hidden="true"></i>
-                        <h6 >Đang Giao Hàng</h6>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <i class="fa fa-check mx-2 fa-3x" aria-hidden="true"></i>
-                        <h6 >Giao Hàng Thành Công</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php require_once 'components/OrderTracking.php'?>
 
-<!--        dia chi nhan hang, hanh trinh don hang-->
-        <div class="justify-content-between row my-3">
-            <div class="col-4 bg-white condition">
-                <h5 class="mt-3 font-weight-bold">Địa Chỉ Giao Hàng</h5>
-                <h6 class="mt-3 font-weight-light">Thiết KwangTuPTIT</h6>
-                <h6 class="mt-3 font-weight-bolder text-warning">0888686868</h6>
-                <h6 class="mt-3 font-weight-light font-italic">Hồ Gươm Plaza Hà Đông, Trần Phú, Mộ Lao, Hà Đông, Hà Nội</h6>
-            </div>
-            <div class="col-7 bg-white condition">
-                <h5 class="mt-3 font-weight-bold">Hành Trình Đơn Hàng</h5>
 
-                    <table class="table">
-                        <tbody>
-                        <tr>
-                            <th scope="row" class="text-primary">
-                               01/10/21 23:20
-                            </th>
-                            <td colspan="2">Đơn hàng được xác nhận</td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="text-primary">
-                                02/10/21 07:40
-                            </th>
-                            <td colspan="2">Kho chuẩn bị hàng</td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="text-primary">
-                               05/10/21 20:05
-                            </th>
-                            <td colspan="2">Hoàn tất kiểm tra</td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="text-primary">
-                            <td colspan="2">Chờ đơn vị vận chuyển</td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="text-primary">
-                                06/10/21 09:05
-                            </th>
-                            <td colspan="2">Đơn vị vận chuyển nhận hàng và giao cho khách hàng</td>
-                        </tr>
-                        </tbody>
-                    </table>
-
-            </div>
-        </div>
         <div class="condition bg-white mb-5">
             <h5 class="mt-3 font-weight-bold">Chi Tiết Đơn Hàng</h5>
             <table class="table">
--- components/OrderTracking.php
+++ components/OrderTracking.php
@@ -1,1 +1,99 @@
-<?php
+
+<div class="bg-white condition">
+    <div class="row d-flex justify-content-between px-3 top">
+        <div class="d-flex font-weight-bolder">
+            <h5>Đơn Hàng <span class="text-primary font-weight-bold"><?php echo $order['id']?></span></h5>
+        </div>
+        <div class="d-flex flex-column text-sm-right font-weight-bolder font-italic">
+            <p class="mb-0">Giao hàng dự kiến <span><?php echo $order['intendTime'] ?></span></p>
+        </div>
+    </div>
+    <!-- Add class 'active' to progress -->
+    <div class="mt-5 mx-5">
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: <?php
+                    switch ($order['status']){
+                        case '0':
+                            echo '25%';
+                            break;
+                        case '1':
+                            echo '50%';
+                            break;
+                        case '2':
+                            echo '75%';
+                            break;
+                        case '3':
+                            echo '100%';
+                            break;
+                    }
+            ?>"></div>
+        </div>
+        <div class="d-flex justify-content-between mt-4 font-weight-light ">
+            <div class="d-flex align-items-center">
+                <i class="fa fa-check-circle-o mx-2 fa-3x" aria-hidden="true"></i>
+                <h6 >Chờ xác nhận</h6>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fa fa-hand-o-left mx-2 fa-3x" aria-hidden="true"></i>
+                <h6 >Đang chuẩn bị hàng</h6>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fa fa-truck mx-2 fa-3x" aria-hidden="true"></i>
+                <h6 >Đang Giao Hàng</h6>
+            </div>
+            <div class="d-flex align-items-center">
+                <i class="fa fa-check mx-2 fa-3x" aria-hidden="true"></i>
+                <h6 >Giao Hàng Thành Công</h6>
+            </div>
+        </div>
+    </div>
+</div>
+<!--        dia chi nhan hang, hanh trinh don hang-->
+<div class="justify-content-between row my-3">
+    <div class="col-4 bg-white condition">
+        <h5 class="mt-3 font-weight-bold">Địa Chỉ Giao Hàng</h5>
+        <h6 class="mt-3 font-weight-light"><?php echo $order['clientName']?></h6>
+        <h6 class="mt-3 font-weight-bolder text-warning"><?php echo $order['phoneNum']?></h6>
+        <h6 class="mt-3 font-weight-light font-italic"><?php echo $order['shippingAdd']?></h6>
+    </div>
+    <div class="col-7 bg-white condition">
+        <h5 class="mt-3 font-weight-bold">Hành Trình Đơn Hàng</h5>
+
+        <table class="table">
+            <tbody>
+            <?php
+                foreach ($orderSta as $sta):
+            ?>
+                    <tr>
+                        <th scope="row" class="text-primary">
+                            <?php echo $sta['time']?>
+                        </th>
+                        <td colspan="2"><?php
+                            switch ($sta['status']){
+                                case "0":
+                                    echo "Chờ xác nhận";
+                                    break;
+                                case "1":
+                                    echo "Kho xác nhận, bắt đầu đóng gói";
+                                    break;
+                                case "2":
+                                    echo "Hoàn tất đóng gói, chuyển cho đơn vị vận chuyển";
+                                    break;
+                                case "3":
+                                    echo "Giao hàng thành công";
+                                    break;
+                                case "4";
+                                    echo "Đã hủy";
+                                    break;
+                            }
+                            ?></td>
+                    </tr>
+            <?php
+                endforeach;
+            ?>
+
+            </tbody>
+        </table>
+
+    </div>
+</div>
